c1cd0fe29fd71dc3e511d3b58c815eb5e6d9ec74
daheeahn <deg9810@gmail.com>
2022-10-22 14:17:37 +0900

    [ch3 - 3 - 컴포넌트와 prop: 실습] 시작 전 코드

--- App.js
+++ App.js
@@ -1,59 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-const Header = (props) => {
-  console.log("props of Header", props);
-  return <Text>{props.title}</Text>;
-};
-const MyProfile = () => {
-  return <Text>MyProfile</Text>;
-};
-const Division = () => {
-  return <Text>Division</Text>;
-};
-const FriendSection = () => {
-  return <Text>FriendSection</Text>;
-};
+import { StyleSheet, View } from "react-native";
 
 /**
- * @description Functional Component
+ * Header
+ * MyProfile
+ * Division
+ * FriendSection
+ * FriendList
  */
-const Profile = (props) => {
-  return <Text>{props.name}</Text>;
-};
-/**
- * @description Class Component
- */
-// class Profile extends React.Component {
-//   render() {
-//     return <Text>{this.props.name}</Text>;
-//   }
-// }
-const FreindList = () => {
-  return (
-    <View>
-      <Profile name="민호" />
-      <Profile name="지연" />
-      <Profile name="예지" />
-      <Profile name="지연" />
-      <Profile name="세인" />
-      <Profile name="유림" />
-      <Profile name="지현" />
-      <Profile name="현서" />
-    </View>
-  );
-};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Header title="친구" />
-      <MyProfile />
-      <Division />
-      <FriendSection />
-      <FreindList />
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
