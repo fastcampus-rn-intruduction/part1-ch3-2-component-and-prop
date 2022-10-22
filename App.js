a9bb33ecb4dec9411ab038b4fddcacca6e5b180f
daheeahn <deg9810@gmail.com>
2022-10-22 14:10:19 +0900

    [ch3 - 2 - 컴포넌트와 prop: 이론] 최종코드

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Header = (props) => {
+  console.log("props of Header", props);
+  return <Text>{props.title}</Text>;
+};
+const MyProfile = () => {
+  return <Text>MyProfile</Text>;
+};
+const Division = () => {
+  return <Text>Division</Text>;
+};
+const FriendSection = () => {
+  return <Text>FriendSection</Text>;
+};
+
+/**
+ * @description Functional Component
+ */
+const Profile = (props) => {
+  return <Text>{props.name}</Text>;
+};
+/**
+ * @description Class Component
+ */
+// class Profile extends React.Component {
+//   render() {
+//     return <Text>{this.props.name}</Text>;
+//   }
+}
+const FreindList = () => {
+  return (
+    <View>
+      <Profile name="민호" />
+      <Profile name="지연" />
+      <Profile name="예지" />
+      <Profile name="지연" />
+      <Profile name="세인" />
+      <Profile name="유림" />
+      <Profile name="지현" />
+      <Profile name="현서" />
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="친구" />
+      <MyProfile />
+      <Division />
+      <FriendSection />
+      <FreindList />
     </View>
   );
 }
@@ -13,8 +59,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+### 패스트캠퍼스 React Native 앱 개발
+
+> Part 1
+
+> Chapter 3 - 2,3 - 컴포넌트와 prop 이론,실습 코드입니다.
